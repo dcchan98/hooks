b6320c36961a0e8237236d2f1b2d962adb9476f9
Sean Chan <dcchan98@gmail.com>
2021-01-13 14:42:36 +0800

    handle class naming by factorising out css names in semantic ui

--- src/components/AccordionItem.js
+++ src/components/AccordionItem.js
@@ -3,14 +3,21 @@
 import React from "react";
 
 export default function AccordionItem(props) {
+
+  // toggle from active to inactive ( vice versa) to handle dropdown
+  const activityState = "active"
+  const contentIsActive = `content ${activityState}`
+
+
 	return (
 		<React.Fragment key = {props.hero.name}>
 			<div onClick={props.handleClick} className='title active'>
 				<i className='dropdown icon'></i>
 				{props.hero.name}
 			</div>
-			<div className='content active'>
-				<p className='transition hidden'>
+      {/* Add active class below to show content */}
+			<div className={contentIsActive}>
+				<p className=''>
         {props.hero.description}
 				</p>
 			</div>
